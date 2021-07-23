38b86b85d3fe463e9ab8625ce6e4ba9d7bc081a8
Kepa Mola <k.mola@e-coordina.com>
2021-07-23 13:57:59 +0200

    Added add-to-cart func.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,16 +2,21 @@ import { CartActionTypes } from "./cart.types";
 
 const initialState = {
   hidden: true,
+  cartItems: []
 };
 
-const cartReducer = (state = initialState, { type }) => {
+const cartReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden,
       };
-
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, payload]
+      }
     default:
       return state;
   }
